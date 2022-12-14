67e92701f7d106bf963d0782a5b90f128a69e8fe
Basil Kraft <29065166+bask0@users.noreply.github.com>
2022-12-14 12:27:39 +0100

    add conda prettytable

--- create_env.sh
+++ create_env.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# conda create --name dml python=3.10 pytorch torchvision torchaudio cudatoolkit=11.3 netcdf4 xarray pandas zarr rasterio matplotlib cartopy cdo nco texlive-core dask jupyterlab ipywidgets numpy tqdm pytorch-lightning ffmpeg imageio scikit-learn optuna flake8 parallel tmux pangeo-forge-recipes python-graphviz -c pytorch conda-forge
-
 echo ">>> Installing conda packages"
 
-conda create --yes --name dml python=3.10 netcdf4 xarray pandas zarr rasterio matplotlib cartopy cdo nco texlive-core dask jupyterlab ipywidgets numpy tqdm pytorch-lightning ffmpeg imageio scikit-learn wandb flake8 parallel tmux python-graphviz -c pytorch -c conda-forge
+conda create --yes --name dml python=3.10 netcdf4 xarray pandas zarr rasterio matplotlib cartopy cdo nco texlive-core dask jupyterlab ipywidgets numpy tqdm pytorch-lightning ffmpeg imageio scikit-learn wandb flake8 parallel tmux python-graphviz prettytable -c pytorch -c conda-forge
 
 echo ">>> Installing pip packages"
 
