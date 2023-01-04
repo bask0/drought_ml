fc565201d29b7e9d45360708426b8dab56675a97
Basil Kraft <29065166+bask0@users.noreply.github.com>
2023-01-04 14:30:27 +0100

    wip

--- project/run_model.py
+++ project/run_model.py
@@ -3,24 +3,30 @@
 import pytorch_lightning as pl
 from project.pl_models.tcn_model import TemporalConvNetPL
 from dataset import GeoDataQueue
+from utils.pl_utils import OutputWriter
+
+
 
 # from pytorch_lightning.profilers import AdvancedProfiler, PyTorchProfiler
 
-# import logging; logging.getLogger('pytorch_lightning').setLevel(logging.ERROR)
+#import logging; logging.getLogger('pytorch_lightning').setLevel(logging.ERROR)
 
 # profiler = AdvancedProfiler(dirpath='.', filename='perf_logs')
 # profiler = PyTorchProfiler(dirpath='.', filename='perf_logs', group_by_input_shapes=True)
 
 # TODO:
-# - NLL loss
-# - Save rpedictions
+# - Save predictions
 # - Predict anomalies instead of raw
-# - SLURM integration
+# - Fix (too?) fast convergence
+# - NLL loss
 # - Set up fast dev run
+# - SLURM integration
+
 
 geodata = GeoDataQueue(
     fold_id=0,
-    batch_size=4,
+    target_daily=['fvc'],
+    batch_size=5,
     chunk_buffer_size=4,
     num_queue_workers=12,
 )
@@ -39,14 +45,21 @@ tcn = TemporalConvNetPL(
     weight_decay=0.0
 )
 
+output_writer = OutputWriter(targets=geodata.targets)
+
 trainer = pl.Trainer(
     accelerator='gpu',
     devices=[7],
     num_sanity_val_steps=0,
     log_every_n_steps=10,
     accumulate_grad_batches=5,
+    limit_train_batches=20,
+    limit_val_batches=5,
+    callbacks=[output_writer],
     # overfit_batches=5,
     # profiler=profiler,
     # fast_dev_run=2,
 )
+
 trainer.fit(tcn, datamodule=geodata)
+trainer.predict(tcn, datamodule=geodata, return_predictions=False)
