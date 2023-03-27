63113ff482aae22605de20b0f48ad01ecf414ca7
Basil Kraft <29065166+bask0@users.noreply.github.com>
2023-03-27 13:19:02 +0200

    rework preprocessing

--- preprocessing/cube_harmonize.py
+++ preprocessing/cube_harmonize.py
@@ -3,7 +3,7 @@ import sys
 import os
 from argparse import ArgumentParser
 
-from preprocessing.cube_harmonize_utils import create_dummy, write_data, add_anomalies, merge_stats
+from preprocessing.cube_harmonize_utils import create_dummy, write_data
 
 
 if __name__ == '__main__':
@@ -34,20 +34,8 @@ if __name__ == '__main__':
         '-f',
         '--in_file',
         type=str,
-        required=('--create' not in sys.argv) and ('--anomalies' not in sys.argv) and ('--seasonality' not in sys.argv) and ('--merge_stats' not in sys.argv),
+        required=('--create' not in sys.argv),
         help='dataset to add to the cube. Only required if none `--create`, `--anomalies`, and `--merge_sats` are passed.')
-    parser.add_argument(
-        '--anomalies',
-        type=str,
-        help='if passed, anomalies are calculated for the given variable. One of `lst`, `fvc`.')
-    parser.add_argument(
-        '--seasonality',
-        type=str,
-        help='if passed, seasonality is calculated for the given variable. One of `t2m`, `tp`, `ssrd`, `rH_cf`.')
-    parser.add_argument(
-        '--merge_stats',
-        action='store_true',
-        help='if passed, the standard deviation and mean are calculated from the `<var>_stats` variables.')
     parser.add_argument(
         '--dryrun',
         action='store_true',
@@ -64,19 +52,5 @@ if __name__ == '__main__':
             raise FileNotFoundError(
                 f'`out_path={args.out_file}` is not a directory. '
                 'Initialize the dataset first with flag `--create_dataset`.')
-        if args.anomalies is None and args.seasonality is None and args.merge_stats is False:
-            write_data(in_path=args.in_file, out_path=args.out_file, dryrun=args.dryrun)
-        elif args.anomalies is not None:
-            if args.anomalies not in ('lst', 'fvc'):
-                raise ValueError(
-                    f'argument `anomalies` must be one of (\'fvc\', \'lst\'), is {args.anomalies}.'
-                )
-            add_anomalies(var=args.anomalies, out_path=args.out_file, dryrun=args.dryrun, num_proc=12)
-        elif args.seasonality is not None:
-            if args.seasonality not in ('t2m', 'tp', 'ssrd', 'rh_cf'):
-                raise ValueError(
-                    f'argument `anomalies` must be one of (\'t2m\', \'tp\', \'ssrd\', \'rh_cf\'), is {args.seasonality}.'
-                )
-            add_anomalies(var=args.seasonality, out_path=args.out_file, dryrun=args.dryrun, num_proc=12, msc_only=True)
-        else:
-            merge_stats(out_path=args.out_file)
+
+        write_data(in_path=args.in_file, out_path=args.out_file, dryrun=args.dryrun)
