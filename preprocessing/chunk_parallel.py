df98d1632b741fb2c7188d436050b12e1f743575
Basil Kraft <29065166+bask0@users.noreply.github.com>
2023-04-21 14:20:40 +0200

    improve chunk_parellel

--- preprocessing/chunk_parallel.py
+++ preprocessing/chunk_parallel.py
@@ -36,10 +36,12 @@ mpp.Pool.istarmap = istarmap
 
 def chunk_parcall(
         path: str,
-        var: str,
+        var: str | list[str],
         num_processes: int,
         fun: Callable,
         use_mask: bool,
+        mask: xr.DataArray | None = None,
+        xrcombine: bool = False,
         desc: str = 'Processing |',
         args: tuple = ()):
     """Apply function parallelized over data chunks.
@@ -54,6 +56,9 @@ def chunk_parcall(
             else:
                 path, var, lat_slice, lon_slice
         use_mask: if True, only non-masked chunks are processed.
+        mask: if passed, this mask is used if 'use_mask=True'.
+        xrcombine: if True, the results are combined into one dataset. Else, the
+            chunks will be returned as a list (default).
         desc: the tqdm description to desplay before the variable name. Default is
             'Processing |', which displays as 'Processing | my_var'.
         args: tuple passed to `fun(..., *args)` as last positional agruments.
@@ -65,7 +70,10 @@ def chunk_parcall(
     cube = xr.open_zarr(path)
 
     if use_mask:
-        mask = cube.mask.load()
+        if mask is None:
+            mask = cube.mask.load()
+        else:
+            mask = mask.load()
 
     da = cube[var]
 
@@ -75,6 +83,7 @@ def chunk_parcall(
         np.concatenate((np.zeros(1, dtype=int), np.cumsum(da.chunksizes['lon']))), 2)
 
     iterable = []
+    masked = []
 
     for lat_chunk_bound in lat_chunk_bounds:
         for lon_chunk_bound in lon_chunk_bounds:
@@ -87,13 +96,25 @@ def chunk_parcall(
             else:
                 iterable.append((path, var, lat_slice, lon_slice, *args))
 
+    if isinstance(var, list):
+        desc_var = ', '.join(var)
+    else:
+        desc_var = var
+
     results = []
     with Pool(num_processes) as pool:
         for r in tqdm.tqdm(
                 pool.istarmap(fun, iterable),
                 total=len(iterable),
-                desc=f'{desc} {var:<18}',
-                miniters=200):
+                desc=f'{desc} {desc_var:<25}',
+                ncols=160):
             results.append(r)
+
+    if xrcombine:
+        results = xr.combine_by_coords(results)
+        sel_dims = {dim: 0 for dim in set(da.dims) - {'lat', 'lon'}}
+        results = results.broadcast_like(da.isel(**sel_dims))
+        if results.lat[0] < results.lat[1]:
+            results = results.reindex(lat=list(reversed(results.lat)))
 
     return results
